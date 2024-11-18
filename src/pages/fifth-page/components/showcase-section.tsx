59931a017794a7fa5106ff5fbb32bc410a919eed
pratham-ts <pratham.thakkar@talentsystems.com>
2024-11-18 18:21:23 +0530

    fixed responsive css

--- src/pages/fifth-page/components/showcase-section.tsx
+++ src/pages/fifth-page/components/showcase-section.tsx
@@ -3,18 +3,26 @@ import React from "preact/compat";
 interface ShowcaseSectionProps {
   title: string;
   description: string;
+  titleClassName?: string;
+  descriptionClassName?: string;
 }
 
 const ShowcaseSection: React.FC<ShowcaseSectionProps> = ({
   title,
   description,
+  descriptionClassName,
+  titleClassName,
 }) => {
   return (
     <div className="flex flex-col">
-      <p className="font-semibold text-base leading-normal text-[#270530] font-work-sans">
+      <p
+        className={`font-semibold text-base leading-normal text-[#FFFAEB] font-work-sans ${titleClassName}`}
+      >
         {title}
       </p>
-      <p className="text-base leading-normal text-[#270530] font-work-sans text-justify">
+      <p
+        className={`text-base leading-normal text-[#FFFAEB] font-work-sans text-left ${descriptionClassName}`}
+      >
         {description}
       </p>
     </div>
--- src/pages/sixth-page.tsx
+++ src/pages/sixth-page.tsx
@@ -4,7 +4,7 @@ const SixthPage = () => {
       id="about-us"
       className="screen5 w-full py-8 md:py-20 md:px-24 bg-[#51025E] text-[#fffaeb]"
     >
-      <div className="flex flex-col gap-20 md:gap-0 md:flex-row items-center">
+      <div className="flex flex-col gap-8 md:gap-0 md:flex-row items-center">
         <text className=" text-2xl font-bold font-['Work Sans'] md:hidden">
           About Us
         </text>
