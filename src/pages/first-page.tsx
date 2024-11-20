36f188fb299080a07537da3f6f9af8633e7e76e3
pratham-ts <pratham.thakkar@talentsystems.com>
2024-11-20 15:29:52 +0530

    sing aos

--- src/pages/first-page.tsx
+++ src/pages/first-page.tsx
@@ -1,29 +1,7 @@
-import { useRef } from "react";
 import { Helmet } from "react-helmet";
 import { useInView } from "react-intersection-observer";
-import useIntersectionObserver from "./reusableHooks";
 
 const FirstPage = () => {
-  const [headingRef, isHeadingInview] = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
-
-  // const animationClass = inView ? "animate-fade-right" : "";
-  const paragraphRef = useRef(null);
-  const imageRef = useRef(null);
-
-  useIntersectionObserver(
-    paragraphRef,
-    "animate-fade-right",
-    "animate-delay-[500ms]"
-  );
-  useIntersectionObserver(
-    imageRef,
-    "animate-fade-left",
-    "animate-duration-2000"
-  );
-
   return (
     <div
       id="home"
@@ -38,16 +16,14 @@ const FirstPage = () => {
       <div className="lg:flex justify-center lg:w-1/2 h-full">
         <div className=" flex flex-col justify-center gap-11 lg:items-start items-center p-3 mb-3 lg:p-0 lg:mb-0">
           <h1
-            ref={headingRef}
-            className={`text-3xl md:text-4xl lg:text-[50px] font-bold font-work-sans ${
-              isHeadingInview ? "animate-fade-right" : ""
-            }`}
+            data-aos="fade-right"
+            className={`text-3xl md:text-4xl lg:text-[50px] font-bold font-work-sans`}
           >
             Spotlight is now in India
           </h1>
 
           <h2
-            ref={paragraphRef}
+            data-aos="fade-right"
             className="text-sm md:text-lg lg:text-[22px] font-wix-madefor-display font-normal  flex flex-col gap-1 lg:gap-2"
           >
             <span>Spotlight - The home of casting is now in India.</span>
@@ -59,7 +35,10 @@ const FirstPage = () => {
       </div>
       <div className="flex justify-center">
         <img
-          ref={imageRef}
+          data-aos="fade-up"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
           src="1st-screen.svg"
           alt="1st screen image"
           className="w-full h-[80%] md:h-[50%] lg:h-screen object"
