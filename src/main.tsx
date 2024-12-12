843387267b36ef03b6100f33f64a8d7def929c90
Samir Patel <samir.patel@talentsystems.com>
2024-12-12 12:29:05 +0530

    GTM added

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.tsx'
+import { render } from "preact";
+import "./index.css";
+import { App } from "./app.tsx";
+import TagManager from "react-gtm-module";
 
-render(<App />, document.getElementById('app')!)
+const tagManagerArgs = {
+  gtmId: "GTM-PFP5754J",
+};
+
+TagManager.initialize(tagManagerArgs);
+
+render(<App />, document.getElementById("app")!);
